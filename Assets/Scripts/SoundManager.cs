a5646af3178b1c840b861c2bcc55a499508eb215
Ukropchik <vova.chakalov.2003@gmail.com>
2023-01-14 23:55:16 +0300

    add memu music

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip[] Sounds;
+
+    public AudioSource Sound;
+
+
+    public void OnSound()
+    { 
+        int n=Random.Range(0, Sounds.Length);
+        Sound.clip = Sounds[n]; 
+        Sound.Play();
+    }
+
+    public void OnEnable()
+    {
+        this.OnSound();
+    }
+}
